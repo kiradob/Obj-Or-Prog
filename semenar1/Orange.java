9f935c3c62c485f15189cd6d8b10ac204e431a10
kira.dob <arik.dobrynina@yandex.ru>
2023-06-25 15:37:03 +0700

    added semenar1 Orange

---
+++ semenar1/Orange.java
@@ -0,0 +1,14 @@
+package semenar1;
+
+
+public class Orange extends Product {
+    public Orange(String kind, double price, Long id) {
+        super(kind, price, id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("У апельсина: %s, %s, %s", getKind(), getPrice(), getId());
+    }
+
+}
