0ececc30abaa5360f44a12bdd01f570fb14b6887
Kira Dobrynina <116497635+kiradob@users.noreply.github.com>
2024-01-09 09:04:27 +0700

    added homework3
    added homework3

---
+++ homework3/Student.java
@@ -0,0 +1,21 @@
+package seminar3;
+
+import lombok.*;
+
+//@Getter
+//@AllArgsConstructor
+//@Setter
+//@ToString
+//@EqualsAndHashCode
+@Data
+public class Student implements Comparable<Student> {
+
+    private final Long id;
+    private final String fullName;
+
+    @Override
+    public int compareTo(Student o) {
+        return fullName.compareTo(o.fullName);
+    }
+}
+
